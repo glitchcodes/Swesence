a197d65b6ce69c84f7b49df6db9d593d8530cd89
Vincent Tampos <binstampos@gmail.com>
2022-09-03 11:22:10 +0800

    updated scripts and config for consistent development across platforms

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,20 @@ import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: path.resolve(__dirname, './dist/'),
-  build: {
+export default defineConfig(({ command }) => {
+  const baseUrl = command === 'serve' ? '/dist/' : path.resolve(__dirname, './dist/')
+
+  return {
+    base: baseUrl,
+    build: {
       outDir: './dist/'
-  },
-  plugins: [
+    },
+    plugins: [
       vue(),
       ElementPlus()
-  ],
-  server: {
+    ],
+    server: {
       port: 3000
+    }
   }
 })
